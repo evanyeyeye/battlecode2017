74c5052d8630a0a86e0b384817f11d82e2540a3f
William Zhang <willzhang05@gmail.com>
2017-01-20 01:27:04 -0500

    Add some code for dodge and directionTo

--- draft_1/Direct.java
+++ draft_1/Direct.java
@@ -19,20 +19,114 @@ public class Direct {
         if(BEFORE_SEND > broadcastingLocations.length)
             return null;
         for(int i = 1; i <= BEFORE_SEND; i++){
-            if(broadcastingLocations[i].distanceTo(broadcastingLocations[i-1]) < 10.0)
-                send = true;
-            else {
-                send = false;
-                break;
-            }
+            send = broadcastingLocations[i].distanceTo(broadcastingLocations[i-1]) < 10.0;
+            if(!send) break;
         }
-        if(send)
-            return rc.getLocation().directionTo(broadcastingLocations[0]);
-        else
-            return null;
+        return send ? rc.getLocation().directionTo(broadcastingLocations[0]) : null;
     }
 
+    public static Direction directionToRobot(RobotInfo robot) {
+    	return new Direction((float)Math.atan((robot.location.x-rc.getLocation().x)/(robot.location.y-rc.getLocation().y)));
+    }
+    
     public static boolean retreat(){
-        return rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent()).length > rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam()).length;
+    	 return rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam()).length < rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent()).length;
+    }
+    
+    public static boolean dodge() {
+    	BulletInfo[] bulletLocations = rc.senseNearbyBullets();
+    	float hp = rc.getHealth();
+    	for(int i = 0; i < bulletLocations.length; i++) {
+    		if(willCollideWithMe(bulletLocations[i])) {
+    			//tryMove(new Direction((float)Math.atan((bulletLocations[i].location.x-rc.getLocation().x)/(bulletLocations[i].location.y-rc.getLocation().y))).opposite());
+    			while(!rc.hasMoved()) {
+	    			try {
+	    				if(tryMove(rc.getLocation().directionTo(bulletLocations[i].location).opposite())) {
+	    					break;
+	    				}
+	    			} catch (GameActionException e) {
+	    				System.out.println("Could not move");
+	    			}
+    			}
+    		}
+    	}
+    	return rc.getHealth() < hp; //did robot get hit by bullet or did it successfully dodge
+    }
+    
+    static boolean tryMove(Direction dir) throws GameActionException {
+        return tryMove(dir,20,3);
+    }
+
+    /**
+     * Attempts to move in a given direction, while avoiding small obstacles direction in the path.
+     *
+     * @param dir The intended direction of movement
+     * @param degreeOffset Spacing between checked directions (degrees)
+     * @param checksPerSide Number of extra directions checked on each side, if intended direction was unavailable
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    static boolean tryMove(Direction dir, float degreeOffset, int checksPerSide) throws GameActionException {
+    	if (rc.hasMoved())
+    		return false;
+        // First, try intended direction
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        }
+        // Now try a bunch of similar angles
+        boolean moved = false;
+        int currentCheck = 1;
+
+        while(currentCheck<=checksPerSide) {
+            // Try the offset of the left side
+            if(rc.canMove(dir.rotateLeftDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateLeftDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // Try the offset on the right side
+            if(rc.canMove(dir.rotateRightDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateRightDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // No move performed, try slightly further
+            currentCheck++;
+        }
+
+        // A move never happened, so return false.
+        return false;
+    }
+
+    /**
+     * A slightly more complicated example function, this returns true if the given bullet is on a collision
+     * course with the current robot. Doesn't take into account objects between the bullet and this robot.
+     *
+     * @param bullet The bullet in question
+     * @return True if the line of the bullet's path intersects with this robot's current position.
+     */
+    public static boolean willCollideWithMe(BulletInfo bullet) {
+        MapLocation myLocation = rc.getLocation();
+
+        // Get relevant bullet information
+        Direction propagationDirection = bullet.dir;
+        MapLocation bulletLocation = bullet.location;
+
+        // Calculate bullet relations to this robot
+        Direction directionToRobot = bulletLocation.directionTo(myLocation);
+        float distToRobot = bulletLocation.distanceTo(myLocation);
+        float theta = propagationDirection.radiansBetween(directionToRobot);
+
+        // If theta > 90 degrees, then the bullet is traveling away from us and we can break early
+        if (Math.abs(theta) > Math.PI/2) {
+            return false;
+        }
+
+        // distToRobot is our hypotenuse, theta is our angle, and we want to know this length of the opposite leg.
+        // This is the distance of a line that goes from myLocation and intersects perpendicularly with propagationDirection.
+        // This corresponds to the smallest radius circle centered at our location that would intersect with the
+        // line that is the path of the bullet.
+        float perpendicularDist = (float)Math.abs(distToRobot * Math.sin(theta)); // soh cah toa :)
+
+        return (perpendicularDist <= rc.getType().bodyRadius);
     }
 }
